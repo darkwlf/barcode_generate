4c09b06a7b4365105c6996d2ac9e776415acc35b
Dark Wolf <63583981+darkwlf@users.noreply.github.com>
2020-12-09 12:48:25 +0330

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,9 @@
+from barcode import Code39
+from barcode.writer import ImageWriter
+import datetime
+import random
+
+price = input("Price:")
+f = open(f'Barcode - {random.randint(1, 100000)}.png', 'ab+')
+Code39(price, writer=ImageWriter()).write(f)
+f.close()
